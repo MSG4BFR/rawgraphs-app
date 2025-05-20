2b727f617b63153a2f70b13e8c753255b69131df
TarGue <taras.guenther@posteo.de>
2025-05-20 08:32:33 +0200

    Add a SPARQL Items JS in order to provide SPARQL examples easily to the UI.

---
+++ src/components/DataLoader/loaders/SPARQLItems.js
@@ -0,0 +1,11 @@
+export const sparqlExamples = [
+  {
+    title: "Standard",
+    query: "SELECT * WHERE { ?s ?p ?o } LIMIT 10",
+  },
+  // Add more examples here if needed, e.g.:
+  // {
+  //   title: "Sum of species",
+  //   query: "SELECT (COUNT(DISTINCT ?species) AS ?numberOfSpecies) WHERE { ?item wdt:P105 ?species . }",
+  // },
+];
